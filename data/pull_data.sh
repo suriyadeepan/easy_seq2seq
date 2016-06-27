e3a3707e0283e80859eb8cd11ddc27a7a239dff4
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2016-06-27 12:39:25 +0530

    add script to pull datasets from the datasets repo

---
+++ data/pull_data.sh
@@ -0,0 +1,4 @@
+wget -c 'https://media.githubusercontent.com/media/suriyadeepan/datasets/master/seq2seq/cornell_movie_corpus/processed_data/test.enc'
+wget -c 'https://media.githubusercontent.com/media/suriyadeepan/datasets/master/seq2seq/cornell_movie_corpus/processed_data/test.dec'
+wget -c 'https://media.githubusercontent.com/media/suriyadeepan/datasets/master/seq2seq/cornell_movie_corpus/processed_data/train.enc'
+wget -c 'https://media.githubusercontent.com/media/suriyadeepan/datasets/master/seq2seq/cornell_movie_corpus/processed_data/train.dec'
